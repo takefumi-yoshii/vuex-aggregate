d4142bfae98e1e374e9ca033d716f28597dbf7f4
takefumi-yoshii <takefumi.yoshii@dena.jp>
2018-07-19 11:07:19 +0900

    fix dispatch return

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,17 +2,17 @@ type KeyMap = { [K: string]: any }
 type A1<T> = T extends (a1: infer I, ...rest: any[]) => any ? I : never
 type A2<T> = T extends (a1: any, a2: infer I, ...rest: any[]) => any ? I : never
 type MT<T> = (state: A1<T>) => void
-type AC = (store: any) => Promise<any>
-type CM = (store: any) => void
-type DP = (store: any) => void
+type CM<T> = (store: any) => void
+type AC<T> = (store: any) => Promise<any>
+type DP<T> = (store: any) => Promise<any>
 type MTPL<T> = (state: A1<T>, payload: A2<T>) => void
-type ACPL<T> = (store: A1<T>, payload: A2<T>) => Promise<any>
-type CMPL<T> = (store, payload: A2<T>) => void
-type DPPL<T> = (store, payload: A2<T>) => void
+type CMPL<T> = (store: any, payload: A2<T>) => void
+type ACPL<T> = (store: any, payload: A2<T>) => Promise<any>
+type DPPL<T> = (store: any, payload: A2<T>) => Promise<any>
 type Mutation<T> = MT<T> | MTPL<T>
-type Action<T> = AC | ACPL<T>
-type Committer<T> = T extends MT<T> ? CM : CMPL<T>
-type Dispatcher<T> = T extends AC ? DP : DPPL<T>
+type Action<T> = AC<T> | ACPL<T>
+type Committer<T> = T extends MT<T> ? CM<T> : CMPL<T>
+type Dispatcher<T> = T extends AC<T> ? DP<T> : DPPL<T>
 type Types<T> = { readonly [K in keyof T]: string }
 type Mutations<T> = { readonly [K in keyof T]: Mutation<T[K]> }
 type Actions<T> = { readonly [K in keyof T]: Action<T[K]> }
